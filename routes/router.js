f83d2359859df7913f8c705a1827fa2442fd2407
alaabashiyi <alaa.bashiy@gmail.com>
2021-10-14 00:22:59 +0300

    added fake data and implemented the get and search methods

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const getItems = require('../middlewares/getItems');
 
-router.get('/api/data', (req, res) => {
-    res.send('Hello World !');
-});
+
+router.get('/api/items', getItems);
 
 
 module.exports = router;
