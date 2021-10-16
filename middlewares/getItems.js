376fab69b087eb3115f6309c7ae660d631a28e72
alaabashiyi <alaa.bashiy@gmail.com>
2021-10-16 17:03:21 +0300

    clean up

--- middlewares/getItems.js
+++ middlewares/getItems.js
@@ -3,10 +3,10 @@ const { search, maxProducts, LIMIT, products, pages } = require('../utils/helper
 const getItems = (req, res, next) => {
     const { keyword = '', page = 1 } = req.query;
     let data;
-
     if (keyword !== '') {
         data = search(keyword).slice(0, LIMIT);
-        return res.json({ data, results: data.length, maxProducts });
+        const pages = Math.ceil(data.length / LIMIT)
+        return res.json({ data, results: data.length, pages });
     }
 
     data = products.slice((page - 1) * LIMIT, page * LIMIT);
